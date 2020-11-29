2c24871b76c7ec7642dfa303d8038563c9312384
binghi97 <andrei.petrescu97@gmail.com>
2020-11-29 21:11:25 +0200

    curs2 challenge

--- curs1/src/curs2/CodeChallenge/Challenge3/TestAnimals.java
+++ curs1/src/curs2/CodeChallenge/Challenge3/TestAnimals.java
@@ -6,4 +6,11 @@ public class TestAnimals {
   Animal a = new Fish();
   Animal e = new Spider();
   Pet p = new Cat();
+  Animal cat = new Cat();
+
+  public static void main(String[] args) {
+    TestAnimals t = new TestAnimals();
+    System.out.println(t.cat.legs);
+    System.out.println(t.c.name);
+  }
 }
